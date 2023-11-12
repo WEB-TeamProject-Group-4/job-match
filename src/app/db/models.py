5847b3bee00186c74bd50e1036dd27a99d4151aa
tihomir-a51 <kamenov.tihomir@gmail.com>
2023-11-12 19:59:27 +0200

    uploading some fixes

--- src/app/db/models.py
+++ src/app/db/models.py
@@ -35,7 +35,9 @@ class DbProfessionals(Base):
     id = Column(String(50), primary_key=True, default=str(uuid.uuid4()))
     first_name = Column(String)
     last_name = Column(String)
+    user_id = Column(String(50), ForeignKey('users.id'))
     user = relationship('DbUsers', back_populates='professional')
+    professional_info_id = Column(String(50), ForeignKey('professional_info.id'))
     professional_info = relationship('DbProfessionalInfo', back_populates='professional')
     jobs_matches = relationship("DbAds", secondary=jobs_matches, backref="professionals")
 
@@ -43,7 +45,9 @@ class DbCompanies(Base):
     __tablename__: str = 'companies'
     id = Column(String(50), primary_key=True, default=str(uuid.uuid4()))
     name = Column(String)
+    user_id = Column(String(50), ForeignKey('users.id'))
     user = relationship('DbUsers', back_populates='company')
+    company_info_id = Column(String(50), ForeignKey('company_info.id'))
     company_info = relationship('DbCompanyInfo', back_populates='company')
     jobs_matches = relationship("DbAds", secondary=jobs_matches, backref="companies")
 
@@ -52,6 +56,7 @@ class DbProfessionalInfo(Base):
     id = Column(String(50), primary_key=True, default=str(uuid.uuid4()))
     status = Column(String)
     professional = relationship('DbProfessionals', back_populates='professional_info')
+    info_id = Column(String(50), ForeignKey('info.id'))
     info = relationship('DbInfo', back_populates='professional_info')
 
 class DbCompanyInfo(Base):
@@ -59,6 +64,7 @@ class DbCompanyInfo(Base):
     id = Column(String(50), primary_key=True, default=str(uuid.uuid4()))
     contacts = Column(String)
     company = relationship('DbCompanies', back_populates='company_info')
+    info_id = Column(String(50), ForeignKey('info.id'))
     info = relationship('DbInfo', back_populates='company_info')
 
 class DbInfo(Base):
@@ -69,6 +75,7 @@ class DbInfo(Base):
     picture = Column(LargeBinary, nullable=True, default=None)
     company_info = relationship('DbCompanyInfo', back_populates='info')
     professional_info = relationship('DbProfessionalInfo', back_populates='info')
+    ad_id = Column(String(50), ForeignKey('ads.id'))
     ads = relationship('DbAds', back_populates='info')
 
 class DbAds(Base):
@@ -77,16 +84,17 @@ class DbAds(Base):
     description = Column(String)
     location = Column(String)
     status = Column(String)
+    salary_range_id = Column(String(50), ForeignKey('salary_range.id'))
     salary_range = relationship('DbSalaryRange', back_populates='ads')
     info = relationship('DbInfo', back_populates='ads')
-    requirements = relationship("DbRequirements", secondary=requirements_has_ads, backref="ads")
+    requirements = relationship("DbRequirements", secondary=requirements_has_ads, backref="ads_associated")
     jobs_matches = relationship("DbProfessionalInfo", secondary=jobs_matches, backref="ads")
 
 class DbRequirements(Base):
     __tablename__ = 'requirements'
     id = Column(String(50), primary_key=True, default=str(uuid.uuid4()))
     name = Column(String)
-    ads = relationship("DbAds", secondary=requirements_has_ads, backref="requirements")
+    ads = relationship("DbAds", secondary=requirements_has_ads, backref="requirements_associated")
 
 class DbSalaryRange(Base):
     __tablename__: str = 'salary_range'
