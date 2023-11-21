47407bd6393cceed73ca27ff2fad18e6e8521dd2
Todor Milushev <toshko_2312@abv.bg>
2023-11-21 18:34:04 +0200

    Add delete_company tests

--- tests/crud/crud_company_test.py
+++ tests/crud/crud_company_test.py
@@ -1,5 +1,7 @@
 import pytest
 
+from fastapi import HTTPException
+
 from app.crud import crud_company
 from app.db.models import DbCompanies, DbUsers
 
@@ -67,3 +69,39 @@ async def test_update_company_crud(db, test_db):
     assert result.id == company.id
     assert result.name == new_company_name
     assert result.contacts == new_company_contact
+
+
+@pytest.mark.asyncio
+async def test_check_company_delete_permission(db, test_db):
+    user, company = await create_dummy_company()
+    db.add(user)
+    db.add(company)
+    db.commit()
+
+    result = await crud_company.check_company_delete_permission(db, company.id, user.id)
+
+    assert result.id == company.id
+    assert result.name == company.name
+
+    # Testing when user is not admin and not owner of the company
+    user.id = 'newDummyId'
+    db.commit()
+
+    with pytest.raises(HTTPException) as exception:
+        await crud_company.check_company_delete_permission(db, company.id, user.id)
+
+    exception_info = exception.value
+    assert exception_info.status_code == 403
+
+
+@pytest.mark.asyncio
+async def test_delete_company_by_id_crud(db, test_db, mocker):
+    user, company = await create_dummy_company()
+    db.add(user)
+    db.add(company)
+    db.commit()
+    mocker.patch('app.crud.crud_company.check_company_delete_permission', return_value=company)
+
+    await crud_company.delete_company_by_id_crud(db, 'dummyId', 'dummyId')
+
+    assert db.query(DbCompanies).all() == []
