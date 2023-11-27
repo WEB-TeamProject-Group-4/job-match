7f6b1988eb7ada75a9de336806d93d6bc8d7eb70
Todor Milushev <toshko_2312@abv.bg>
2023-11-27 18:09:44 +0200

    Add get_image + docstrings

--- src/app/api/api_v1/endpoints/companies.py
+++ src/app/api/api_v1/endpoints/companies.py
@@ -1,6 +1,7 @@
 from typing import Annotated, List, Union
 
 from fastapi import Depends, APIRouter, HTTPException, status, Query, Path, UploadFile, File
+from fastapi.responses import StreamingResponse
 from sqlalchemy.orm import Session
 
 from app.core.auth import get_current_user
@@ -15,6 +16,16 @@ router = APIRouter(tags=['company'])
 
 @router.post('/companies', response_model=company.CompanyCreateDisplay, status_code=status.HTTP_201_CREATED)
 async def create_company(schema: company.CompanyCreate, db: Annotated[Session, Depends(get_db)]):
+    """
+    Create a new company.
+
+    This endpoint allows the creation of a new company using the provided company creation schema.
+
+    :param schema:The schema containing information for creating the company.
+    :param db:The database session dependency.
+    :return:Information about the created company.
+    :raises HTTPException 400: If there are validation errors in the provided schema.
+    """
     return await create_user(db, schema)
 
 
@@ -23,6 +34,18 @@ async def get_companies(db: Annotated[Session, Depends(get_db)],
                         current_user: Annotated[DbUsers, Depends(get_current_user)],
                         name: Annotated[str, Query(description='Optional name search parameter')] = None,
                         page: Annotated[int, Query(description='Optional page number query parameter', ge=1)] = 1):
+    """
+    Retrieve a list of companies.
+
+    This endpoint allows fetching a list of companies based on optional search parameters.
+
+    :param db:The database session dependency.
+    :param current_user:Details about the current user obtained from the authentication token.
+    :param name: Optional parameter for filtering companies by name.
+    :param page:Optional parameter for specifying the page number (default is 1).
+    :return:A list of company displays containing information about each company.
+    :raises HTTPException 401: If the user is not authenticated.
+    """
     companies = await CRUDCompany.get_multi(db, name, page)
     return companies
 
@@ -30,6 +53,16 @@ async def get_companies(db: Annotated[Session, Depends(get_db)],
 @router.get('/companies/{company_id}', response_model=company.CompanyDisplay)
 async def get_company_by_id(db: Annotated[Session, Depends(get_db)],
                             company_id: Annotated[str, Path(description='Mandatory company id path parameter')]):
+    """
+    Retrieve company details by its unique identifier.
+
+    This endpoint allows fetching details of a specific company based on its unique identifier.
+
+    :param db:The database session dependency.
+    :param company_id:The unique identifier of the company.
+    :return:Information about the specified company.
+    :raises HTTPException 404: If no company is found with the provided company_id.
+    """
     return await CRUDCompany.get_by_id(db, company_id)
 
 
@@ -38,6 +71,19 @@ async def update_company(db: Annotated[Session, Depends(get_db)],
                          current_user: Annotated[DbUsers, Depends(get_current_user)],
                          name: Annotated[str, Query(description='Optional name update parameter')] = None,
                          contact: Annotated[str, Query(description='Optional contact update parameter')] = None):
+    """
+    Update company information.
+
+    This endpoint allows updating information for the authenticated user's company.
+
+    :param db:The database session dependency.
+    :param current_user:Details about the current user obtained from the authentication token.
+    :param name:Optional parameter for updating the company name.
+    :param contact:Optional parameter for updating the company contact information.
+    :return:Information about the updated company.
+    :raises HTTPException 401: If the user is not authenticated.
+    :raises HTTPException 403: If the user's account is not verified.
+    """
     if not current_user.is_verified:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
@@ -51,8 +97,18 @@ async def update_company(db: Annotated[Session, Depends(get_db)],
 @router.delete('/companies/{company_id}', status_code=status.HTTP_204_NO_CONTENT)
 async def delete_company(db: Annotated[Session, Depends(get_db)],
                          company_id: Annotated[str, Path(description='Mandatory company id path parameter')],
-                         current_user: Annotated[DbUsers, Depends(get_current_user)]
-                         ):
+                         current_user: Annotated[DbUsers, Depends(get_current_user)]):
+    """
+    Delete a company by its unique identifier.
+
+    This endpoint allows deleting a specific company based on its unique identifier.
+
+    :param db:The database session dependency.
+    :param company_id:The unique identifier of the company to be deleted.
+    :param current_user:Details about the current user obtained from the authentication token.
+    :return:None
+    :raises HTTPException 401: If the user is not authenticated.
+    """
     return await CRUDCompany.delete_by_id(db, company_id, current_user)
 
 
@@ -61,6 +117,18 @@ async def delete_company(db: Annotated[Session, Depends(get_db)],
 async def create_company_info(db: Annotated[Session, Depends(get_db)],
                               current_user: Annotated[DbUsers, Depends(get_current_user)],
                               schema: company.CompanyInfoCreate):
+    """
+     Create additional information for a company.
+
+    This endpoint allows creating additional information for the authenticated user's company.
+
+    :param db:The database session dependency.
+    :param current_user:Details about the current user obtained from the authentication token.
+    :param schema:The schema containing information for creating company information.
+    :return:Information about the created company info.
+    :raises HTTPException 401: If the user is not authenticated.
+    :raises HTTPException 403: If the user's account is not verified.
+    """
     if not current_user.is_verified:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
@@ -71,18 +139,56 @@ async def create_company_info(db: Annotated[Session, Depends(get_db)],
         return info
 
 
-@router.post('/companies/info/upload', response_model=company.CompanyInfoCreate)
+@router.post('/companies/info/upload')
 async def upload(db: Annotated[Session, Depends(get_db)],
                  current_user: Annotated[DbUsers, Depends(get_current_user)],
-                 image: Annotated[UploadFile, File()]):
+                 image: Annotated[UploadFile, File()]) -> StreamingResponse:
+    """
+    Upload an image for company information.
+
+    This endpoint allows uploading an image for the company info associated with the authenticated user's company.
+
+    :param db:The database session dependency.
+    :param current_user:Details about the current user obtained from the authentication token.
+    :param image:The uploaded image file.
+    :return:A streaming response for the uploaded image.
+    :raises HTTPException 401: If the user is not authenticated.
+    """
     f = await image.read()
     b = bytearray(f)
     return await CRUDCompany.upload(db, current_user.company[0].info_id, b)
 
 
+@router.get('/companies/info/image')
+async def get_image(db: Annotated[Session, Depends(get_db)],
+                    current_user: Annotated[DbUsers, Depends(get_current_user)]) -> StreamingResponse:
+    """
+    Retrieve the image associated with company information.
+
+    This endpoint allows fetching the image associated with the company information of the authenticated user's company.
+
+    :param db:The database session dependency.
+    :param current_user:Details about the current user obtained from the authentication token.
+    :return:A streaming response for the company information image.
+    :raises HTTPException 401: If the user is not authenticated.
+    """
+    return await CRUDCompany.get_image(db, current_user.company[0].info_id)
+
+
 @router.get('/companies/info/', response_model=company.CompanyInfoDisplay)
 async def get_company_info(db: Annotated[Session, Depends(get_db)],
                            current_user: Annotated[DbUsers, Depends(get_current_user)]):
+    """
+    Retrieve additional information about a company.
+
+    This endpoint allows fetching additional information about the authenticated user's company.
+
+    :param db:The database session dependency.
+    :param current_user:Details about the current user obtained from the authentication token.
+    :return:Information about the company.
+    :raises HTTPException 403: If the user's account is not verified.
+    :raises HTTPException 401: If the user is not authenticated.
+    """
     if not current_user.is_verified:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
@@ -98,8 +204,20 @@ async def get_company_info(db: Annotated[Session, Depends(get_db)],
 async def update_info(db: Annotated[Session, Depends(get_db)],
                       current_user: Annotated[DbUsers, Depends(get_current_user)],
                       description: Annotated[str, Query(description='Optional description update parameter')] = None,
-                      location: Annotated[str, Query(description='Optional location update parameter')] = None
-                      ):
+                      location: Annotated[str, Query(description='Optional location update parameter')] = None):
+    """
+    Update additional information for a company.
+
+    This endpoint allows updating the additional information for the authenticated user's company.
+
+    :param db:The database session dependency.
+    :param current_user:Details about the current user obtained from the authentication token.
+    :param description:Optional parameter for updating the company information description.
+    :param location:Optional parameter for updating the company information location.
+    :return:Information about the updated company info.
+    :raises HTTPException 401: If the user is not authenticated.
+    :raises HTTPException 403: If the user's account is not verified.
+    """
     if not current_user.is_verified:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
@@ -113,6 +231,16 @@ async def update_info(db: Annotated[Session, Depends(get_db)],
 @router.delete('/companies/info/{info_id}', status_code=status.HTTP_204_NO_CONTENT)
 async def delete_info(db: Annotated[Session, Depends(get_db)],
                       current_user: Annotated[DbUsers, Depends(get_current_user)],
-                      info_id: Annotated[str, Path(description='Mandatory info id path parameter')]
-                      ):
+                      info_id: Annotated[str, Path(description='Mandatory info id path parameter')]):
+    """
+    Delete additional information for a company by its unique identifier.
+
+    This endpoint allows deleting the info for the authenticated user's company based on its unique identifier.
+
+    :param db:The database session dependency.
+    :param current_user:Details about the current user obtained from the authentication token.
+    :param info_id:The unique identifier of the company information to be deleted.
+    :return:None
+    :raises HTTPException 401: If the user is not authenticated.
+    """
     return await CRUDCompany.delete_info_by_id(db, info_id, current_user)
--- src/app/crud/crud_company.py
+++ src/app/crud/crud_company.py
@@ -1,30 +1,54 @@
+import io
 from typing import Type, TypeVar, Generic, Union
 
 from sqlalchemy.orm import Session
 from fastapi import HTTPException, status
+from fastapi.responses import StreamingResponse
 
 from app.db.models import DbCompanies, DbUsers, DbInfo, DbAds, DbJobsMatches
 from app.schemas.company import CompanyInfoCreate, CompanyInfoDisplay
 
 CompanyModelType = TypeVar('CompanyModelType', bound=Union[Type[DbCompanies], DbCompanies])
 UserModelType = TypeVar('UserModelType', bound=Union[Type[DbUsers], DbUsers])
-InfoModel = TypeVar('InfoModel', bound=Union[DbInfo, Type[DbInfo]])
+InfoModelType = TypeVar('InfoModelType', bound=Union[DbInfo, Type[DbInfo]])
+AdModelType = TypeVar('AdModelType', bound=Union[DbAds, Type[DbAds]])
 
 
-class CRUDCompany(Generic[CompanyModelType, InfoModel, UserModelType]):
+class CRUDCompany(Generic[CompanyModelType, InfoModelType, UserModelType]):
     @staticmethod
     async def get_multi(db: Session, name: str | None, page: int) -> list[CompanyModelType]:
+        """
+         Retrieve a list of companies based on optional search parameters.
+
+        This method queries the database to fetch a list of companies based on the provided search parameters.
+
+        :param db:The database session.
+        :param name:Optional parameter for filtering companies by name.
+        :param page:The page number for pagination (starts from 1).
+        :return:A list of company models.
+        """
         queries = [DbUsers.is_verified == True, DbUsers.is_deleted == False]
         if name:
             search = "%{}%".format(name)
             queries.append(DbCompanies.name.like(search))
 
-        companies: list[CompanyModelType] = db.query(DbCompanies).join(DbCompanies.user).filter(*queries).limit(10).offset(
+        companies: list[CompanyModelType] = db.query(DbCompanies).join(DbCompanies.user).filter(*queries).limit(
+            10).offset(
             (page - 1) * 10).all()
         return companies
 
     @staticmethod
     async def get_by_id(db: Session, company_id: str) -> CompanyModelType:
+        """
+        Retrieve a company by its unique identifier.
+
+        This method queries the database to fetch a specific company based on its unique identifier.
+
+        :param db:The database session.
+        :param company_id:The unique identifier of the company.
+        :return:The company model for the specified identifier.
+        :raises HTTPException 404: If no company is found with the provided company_id.
+        """
         company: CompanyModelType = db.query(DbCompanies).filter(DbCompanies.id == company_id,
                                                                  DbCompanies.is_deleted == False).first()
         if not company:
@@ -36,6 +60,18 @@ class CRUDCompany(Generic[CompanyModelType, InfoModel, UserModelType]):
 
     @staticmethod
     async def update(db: Session, name: str | None, contact: str | None, user_id: str) -> CompanyModelType:
+        """
+        Update information for a company.
+
+        This method updates information for a company based on the provided parameters.
+
+        :param db:The database session.
+        :param name:Optional parameter for updating the company name.
+        :param contact:Optional parameter for updating the company contact information.
+        :param user_id:The unique identifier of the user associated with the company.
+        :return:The updated company model.
+        :raises HTTPException 404: If no company is found for the provided user_id.
+        """
         company: CompanyModelType = db.query(DbCompanies).filter(DbCompanies.user_id == user_id,
                                                                  DbCompanies.is_deleted == False).first()
         if not company:
@@ -53,6 +89,18 @@ class CRUDCompany(Generic[CompanyModelType, InfoModel, UserModelType]):
 
     @staticmethod
     async def delete_by_id(db: Session, company_id: str, user: UserModelType) -> None:
+        """
+        Delete a company by its unique identifier.
+
+        This method marks a company as deleted in the database based on its unique identifier. Additionally, it checks
+        permissions to ensure that only administrators or the company owner can delete the company.
+        :param db:The database session.
+        :param company_id:The unique identifier of the company to be deleted.
+        :param user:The user attempting to delete the company.
+        :return:None
+        :raises HTTPException 403: If the user does not have sufficient permissions to delete the company.
+        :raises HTTPException 404: If no company is found with the provided company_id.
+        """
         company: CompanyModelType = db.query(DbCompanies).filter(DbCompanies.id == company_id,
                                                                  DbCompanies.is_deleted == False).first()
         if not company:
@@ -66,7 +114,7 @@ class CRUDCompany(Generic[CompanyModelType, InfoModel, UserModelType]):
                 detail='Deletion of the company is restricted to administrators or the company owner.'
             )
         else:
-            resumes:DbAds = db.query(DbAds).filter(DbAds.info_id == company.info_id).all()
+            resumes: AdModelType = db.query(DbAds).filter(DbAds.info_id == company.info_id).all()
             if resumes:
                 for resume in resumes:
                     resume.is_deleted = True
@@ -74,7 +122,17 @@ class CRUDCompany(Generic[CompanyModelType, InfoModel, UserModelType]):
             return
 
     @staticmethod
-    async def create_info(db: Session, company_id: str, schema: CompanyInfoCreate) -> InfoModel:
+    async def create_info(db: Session, company_id: str, schema: CompanyInfoCreate) -> InfoModelType:
+        """
+        Create or update additional information for a company.
+
+        This method creates or updates additional information for a company based on the provided parameters.
+
+        :param db:The database session.
+        :param company_id:The unique identifier of the company.
+        :param schema:The schema containing information for creating or updating company information.
+        :return:The created or updated company information model.
+        """
         company: CompanyModelType = await CRUDCompany.get_by_id(db, company_id)
         if company.info_id and not company.info.is_deleted:
             return await CRUDCompany.update_info(db, company.info_id, schema.description, schema.location)
@@ -88,6 +146,18 @@ class CRUDCompany(Generic[CompanyModelType, InfoModel, UserModelType]):
 
     @staticmethod
     async def get_info_by_id(db: Session, info_id: str, company_id: str) -> CompanyInfoDisplay:
+        """
+        Retrieve additional information about a company by its unique identifier.
+
+        This method queries the database to fetch additional information about a company based on its unique identifier.
+        It also includes additional statistics, such as the number of active job ads and the number of job matches for
+        the company.
+        :param db:The database session.
+        :param info_id:The unique identifier of the company information.
+        :param company_id:The unique identifier of the company.
+        :return:Information about the company's additional information with additional statistics.
+        :raises HTTPException 404: If no company information is found with the provided info_id.
+        """
         info = db.query(DbInfo).filter(DbInfo.id == info_id, DbInfo.is_deleted == False).first()
         if not info:
             raise HTTPException(
@@ -102,8 +172,19 @@ class CRUDCompany(Generic[CompanyModelType, InfoModel, UserModelType]):
                                   number_of_matches=number_of_matches)
 
     @staticmethod
-    async def update_info(db: Session, info_id: str, description: str | None, location: str | None) -> InfoModel:
-        info: InfoModel = db.query(DbInfo).filter(DbInfo.id == info_id, DbInfo.is_deleted == False).first()
+    async def update_info(db: Session, info_id: str, description: str | None, location: str | None) -> InfoModelType:
+        """
+        Update additional information for a company.
+
+        This method updates additional information for a company based on the provided parameters.
+        :param db:The database session.
+        :param info_id:The unique identifier of the company information.
+        :param description:Optional parameter for updating the company information description.
+        :param location:Optional parameter for updating the company information location.
+        :return:The updated company information model.
+        :raises HTTPException 404: If no company information is found with the provided info_id.
+        """
+        info: InfoModelType = db.query(DbInfo).filter(DbInfo.id == info_id, DbInfo.is_deleted == False).first()
         if not info:
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND,
@@ -117,10 +198,22 @@ class CRUDCompany(Generic[CompanyModelType, InfoModel, UserModelType]):
         return info
 
     @staticmethod
-    async def delete_info_by_id(db: Session, info_id: str, user: UserModelType):
+    async def delete_info_by_id(db: Session, info_id: str, user: UserModelType) -> None:
+        """
+        Delete additional information for a company by its unique identifier.
+
+        This method marks additional information for a company as deleted in the database based on its unique identifier.
+        It checks permissions to ensure that only administrators or the company owner can delete the information.
+        :param db:The database session.
+        :param info_id:The unique identifier of the company information to be deleted.
+        :param user:The user attempting to delete the information.
+        :return:Nine
+        :raises HTTPException 403: If the user does not have sufficient permissions to delete the information.
+        :raises HTTPException 404: If no company information is found with the provided info_id.
+        """
         company_id: int = user.company[0].id
         company: CompanyModelType = db.query(DbCompanies).filter(DbCompanies.id == company_id).first()
-        info: InfoModel = db.query(DbInfo).filter(DbInfo.id == info_id).first()
+        info: InfoModelType = db.query(DbInfo).filter(DbInfo.id == info_id).first()
         if not info:
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND,
@@ -137,8 +230,20 @@ class CRUDCompany(Generic[CompanyModelType, InfoModel, UserModelType]):
             return
 
     @staticmethod
-    async def upload(db: Session, info_id: str, image: bytearray) -> InfoModel:
-        info: InfoModel = db.query(DbInfo).filter(DbInfo.id == info_id, DbInfo.is_deleted == False).first()
+    async def upload(db: Session, info_id: str, image: bytearray) -> StreamingResponse:
+        """
+        Upload an image for additional information of a company.
+
+        This method uploads an image for the additional information of a company based on the provided parameters.
+        The image is associated with the company information and stored in the database.
+        :param db:The database session.
+        :param info_id:The unique identifier of the company information.
+        :param image:The image data to be uploaded.
+        :return: A streaming response containing the uploaded image.
+        :raises HTTPException 404: If no company information is found with the provided info_id.
+
+        """
+        info: InfoModelType = db.query(DbInfo).filter(DbInfo.id == info_id, DbInfo.is_deleted == False).first()
         if not info:
             raise HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND,
@@ -146,12 +251,48 @@ class CRUDCompany(Generic[CompanyModelType, InfoModel, UserModelType]):
             )
         info.picture = image
         db.commit()
-        return info
+        return StreamingResponse(io.BytesIO(image), media_type="image/jpeg")
+
+    @staticmethod
+    async def get_image(db: Session, info_id: str) -> StreamingResponse:
+        """
+        Retrieve the image associated with additional information for a company.
+
+        This method retrieves the image associated with the additional information of a company based on
+        the provided parameters.
+        :param db:The database session.
+        :param info_id:The unique identifier of the company information.
+        :return:A streaming response containing the retrieved image.
+        :raises HTTPException 404: If no company information is found with the provided info_id.
+        """
+        info: InfoModelType = db.query(DbInfo).filter(DbInfo.id == info_id, DbInfo.is_deleted == False).first()
+        if not info:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f'Info with id {info_id} does not exist.'
+            )
+        return StreamingResponse(io.BytesIO(info.picture), media_type="image/jpeg")
 
 
 async def is_admin(user: UserModelType) -> bool:
+    """
+    Check if the user has administrative privileges.
+
+    This method checks whether the provided user has administrative privileges based on their user type.
+    :param user:The user model for whom to check administrative privileges.
+    :return:True if the user has administrative privileges, False otherwise.
+    """
     return user.type == 'admin'
 
 
 async def is_owner(company: CompanyModelType, user_id: str) -> bool:
+    """
+    Check if the provided user is the owner of the company.
+
+    This method checks whether the provided user is the owner of the specified company based on their user ID.
+
+    :param company:The company model for which ownership is being checked.
+    :param user_id:The unique identifier of the user to check for ownership.
+    :return:True if the user is the owner of the company, False otherwise.
+    """
     return company.user_id == user_id
