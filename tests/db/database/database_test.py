9ee52ab1b23583ada30b8baffb43741f81f03fb0
tihomir-a51 <kamenov.tihomir@gmail.com>
2023-11-27 10:46:38 +0200

    test_get_db

---
+++ tests/db/database/database_test.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+from app.db.database import get_db
+from tests.conftest import SQLALCHEMY_DATABASE_URL
+
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
+
+# def test_get_db():
+
+#     with get_db(engine) as db:
+#         assert isinstance(db, Session)
+
+
+def test_get_db():
+    expected_db_type = Session
+
+    db_generator = get_db()
+    db = next(db_generator)
+
+    assert isinstance(db, expected_db_type)
+
+    db_generator.close()
